a24c310012da96cd5ea3eac877c922b99ff211d7
bastien <bastien.coutte@efrei.net>
2025-12-18 15:06:59 +0100

    TP complet : Docker, docker-compose, microservices Java/PHP et communication

--- RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
+++ RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
@@ -2,12 +2,22 @@ package com.ingnum.rentalservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class BonjourController {
 
+    private final RestTemplate restTemplate = new RestTemplate();
+
     @GetMapping("/bonjour")
     public String bonjour() {
         return "bonjour";
+    }
+
+    @GetMapping("/bonjour-php")
+    public String bonjourDepuisPhp() {
+        // Appel du microservice PHP via le nom de service docker-compose
+        String prenom = restTemplate.getForObject("http://php-service", String.class);
+        return "bonjour " + prenom;
     }
 }
